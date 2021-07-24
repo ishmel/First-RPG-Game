9000c4d43041fc2a46ba5507b8aefedc28d32944
ishmel <ishmel.smith@live.com>
2021-07-23 21:48:55 -0700

    Update LivingCreature.cs

--- ClassLibrary1/LivingCreature.cs
+++ ClassLibrary1/LivingCreature.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Engine
 {
-    public class LivingCreature
+    public class LivingCreature : INotifyPropertyChanged
     {
         public int CurrentHitPoints { get; set; }
         public int MaximumHitPoints { get; set; }
@@ -16,6 +17,14 @@ namespace Engine
             CurrentHitPoints = currentHitPoints;
             MaximumHitPoints = maximumHitPoints;
            
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
         }
     }
 }
